b632b02de961823be3df72739d9d703891c64207
Taemin Heo <69319840+TaeminHeo@users.noreply.github.com>
2024-09-23 14:14:41 -0400

    Update FRP_jacket.py

--- FRP_jacket.py
+++ FRP_jacket.py
@@ -598,32 +598,31 @@ def ReadOutput(DispIO, DispLS, DispCP, nStory):
     nodeDrift = pd.read_csv("results/nodeDrift.out", sep=" ", header=None).to_numpy()
     EQDrift = np.diff(nodeDrift)/3000
     
-    m ,n = node.shape
-    
-    IndexStep = np.zeros(1,3)
+    m, n = node.shape
+
+    IndexStep = np.zeros(3)
     
     for i in range(1,m):
-        if ( (node[i-1,1]<=DispIO) && (node(i,1)>=DispIO) ):
-            IndexStep[0,0] = i
+        if ( (node[i-1,1]<=DispIO) & (node[i,1]>=DispIO) ):
+            IndexStep[0] = i
             nodeIO = node[i,:]
             DriftIO = EQDrift[i,:]
-            DriftIO[0,0] = max(EQDrift[i,2:(nStory+1)]);
-        end
-        if ( (node(i-1,2)<=DispLS) && (node(i,2)>=DispLS) )  
-            IndexStep(1,2) = i;
-            nodeLS = node(i,:);
-            DriftLS = EQDrift(i,:);
-            DriftLS(1,1) = max(EQDrift(i,2:(nStory+1)));    
-        end
-        if ( (node(i-1,2)<=DispCP) && (node(i,2)>=DispCP) )  
-            IndexStep(1,3) = i;
-            nodeCP = node(i,:);
-            DriftCP = EQDrift(i,:);
-            DriftCP(1,1) = max(EQDrift(i,2:(nStory+1)));  
-            tmp = DriftCP*1000;
-            DriftCP = round(tmp)/1000;
-            break;
-        end
+            DriftIO[0] = max(EQDrift[i,1:(nStory+1)])
+    
+        if ( (node[i-1,1]<=DispLS) & (node[i,1]>=DispLS) ):  
+            IndexStep[1] = i
+            nodeLS = node[i,:]
+            DriftLS = EQDrift[i,:]
+            DriftLS[0] = max(EQDrift[i,1:(nStory+1)])    
+    
+        if ( (node[i-1,1]<=DispCP) & (node[i,1]>=DispCP) ):  
+            IndexStep[2] = i
+            nodeCP = node[i,:]
+            DriftCP = EQDrift[i,:]
+            DriftCP[0] = max(EQDrift[i,1:(nStory+1)])  
+            tmp = DriftCP*1000
+            DriftCP = np.round(tmp)/1000
+            break
 
     return nodeIO, nodeLS, nodeCP, DriftIO, DriftLS, DriftCP, IndexStep
 
